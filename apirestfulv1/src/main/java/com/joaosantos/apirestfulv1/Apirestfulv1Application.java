5594b0ba304050c1f4742aef2f0ba57e9f97d93c
PedroAlmeida08 <74750373+PedroAlmeida08@users.noreply.github.com>
2025-04-07 10:16:10 -0300

    base Spring code

---
+++ apirestfulv1/src/main/java/com/joaosantos/apirestfulv1/Apirestfulv1Application.java
@@ -0,0 +1,13 @@
+package com.joaosantos.apirestfulv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apirestfulv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apirestfulv1Application.class, args);
+	}
+
+}
