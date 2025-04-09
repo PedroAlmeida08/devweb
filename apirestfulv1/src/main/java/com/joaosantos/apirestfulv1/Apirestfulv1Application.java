dde78d31ce66a6507bde547fe5187a2fa5383a5a
PedroAlmeida08 <74750373+PedroAlmeida08@users.noreply.github.com>
2025-04-09 10:49:24 -0300

    post class

--- apirestfulv1/src/main/java/com/joaosantos/apirestfulv1/Apirestfulv1Application.java
+++ apirestfulv1/src/main/java/com/joaosantos/apirestfulv1/Apirestfulv1Application.java
@@ -1,6 +1,8 @@
 package com.joaosantos.apirestfulv1;
 
 import com.joaosantos.apirestfulv1.model.Produto;
+import com.joaosantos.apirestfulv1.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +13,14 @@ import java.time.LocalDate;
 @SpringBootApplication
 public class Apirestfulv1Application implements CommandLineRunner {
 
+	// Quando o método run encontra essa anotação, cria um objeto de uma classe que implementa a
+	// interface ProdutoRepository
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+	// .run() é um método CommandLineRunner e será utilizado para popular banco de dados
 	// .run() coloca no ar o servidor Tomcat
+	// .run() abre e lê as configurações presentes em resources.application.properties
 	public static void main(String[] args) {
 		SpringApplication.run(Apirestfulv1Application.class, args);
 	}
@@ -28,6 +37,12 @@ public class Apirestfulv1Application implements CommandLineRunner {
 				LocalDate.of(2024, 4, 26),
 				"frutas");
 
+		// Se produto for um objeto transiente, .save() chama o método persist da JPA
+		// Se produto for um objeto destacado, .save() chama o método merge da JPA
+		// Em tempo de compilação, .save() é procurado de ProdutoRepository pra cima
+		// Em tempo de execução, .save() é procurado de ProdutoRepositoryImpl e encontra o método .save() de DAOGenericoImpl e o herda
+		produtoRepository.save(produto);
+
 		produto = new Produto(
 				"abobrinha.jpg",
 				"Abobrinha",
@@ -37,6 +52,8 @@ public class Apirestfulv1Application implements CommandLineRunner {
 				BigDecimal.valueOf(1.1),
 				LocalDate.of(2024, 5, 22),
 				"legumes");
+
+		produtoRepository.save(produto);
 
 		System.out.println("Ok!");
 	}
---
+++ apirestfulv1/src/main/java/com/joaosantos/apirestfulv1/repository/ProdutoRepository.java
@@ -0,0 +1,17 @@
+package com.joaosantos.apirestfulv1.repository;
+
+import com.joaosantos.apirestfulv1.model.Produto;
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    // :id --> do produto deve ser igual a um id que será passado por @Param
+    @Query("select p from Produto p where p.id = :id")
+    Optional<Produto> recuperarProdutoPorIdComLock(@Param("id") Long id);
+}
