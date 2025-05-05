f91e5b530526fcc7bdb336783117e98fa1c8da71
PedroAlmeida08 <jp_almeida@id.uff.br>
2025-05-05 09:43:35 -0300

    base code

--- projeto01v2/src/pages/ProdutosPage.tsx
+++ projeto01v2/src/pages/ProdutosPage.tsx
@@ -5,6 +5,31 @@ import recuperarProdutos from "../util/recuperarProdutos";
 
 const ProdutosPage = () => {
   const [produtos, setProdutos] = useState([] as Produto[]);
+  const [erro, setErro] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8080/produtos")
+      .then((response) => {
+        if(!response.ok){
+          throw new Error("Ocorreu um erro ao recuperar produtos. Status code = " + response.status);
+        }
+      // return response.json() as Promise<Produto[]>
+      // as Promise<Produto[]> é uma asserção e, diferente de um cast, 
+      // dá erro em tempo de execução, mas não em tempo de compilação
+      // Faz com que produtos seja um vetor de Produto
+      return response.json() as Promise<Produto[]>
+    })
+    .then((produtos) => {
+      setProdutos(produtos)
+    })
+    .catch((error) => {
+      if (error instanceof Error){
+        setErro(error.message);
+      } else {
+        setErro("Erro desconhecido. Msg = " + error)
+      }
+    })
+  }, [])
 
   // [] indica uma lista de dependências
   // [] indica que o método useEffect só será utilizado uma vez
@@ -20,6 +45,8 @@ const ProdutosPage = () => {
   if (produtos.length === 0)
     return <p className="fw-bold">Carregando produtos ...</p>
 
+  if(erro) <p className="fw-bold">{erro}</p>
+  
   return (
     <>
       <h5>Lista de Produtos</h5>
