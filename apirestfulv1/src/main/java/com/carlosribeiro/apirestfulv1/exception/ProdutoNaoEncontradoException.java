21fcc006324d6dffc19180ed27f9eb27eb43556f
PedroAlmeida08 <jp_almeida@id.uff.br>
2025-05-06 09:14:26 -0300

    Revert "fix: API fetch"
    This reverts commit f7a12b2e8f6e056516cfc5fb380a5a1ba1aaed78.

--- apirestfulv1/src/main/java/com/carlosribeiro/apirestfulv1/exception/ProdutoNaoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package com.carlosribeiro.apirestfulv1.exception;
-
-public class ProdutoNaoEncontradoException extends RuntimeException {
-    public ProdutoNaoEncontradoException(String message) {
-        super(message);
-    }
-}
