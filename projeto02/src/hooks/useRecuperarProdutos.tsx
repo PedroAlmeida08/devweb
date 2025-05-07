23b293fe4865e68b949a17254ad1515899cdf7ec
PedroAlmeida08 <jp_almeida@id.uff.br>
2025-05-07 07:49:28 -0300

    base code

---
+++ projeto02/src/hooks/useRecuperarProdutos.tsx
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+
+const useRecuperarProdutos = () => {
+  const recuperarProdutos = async () => {
+    const response = await fetch("http://localhost:8080/produtos");
+    if (!response.ok) {
+      throw new Error(
+        "Ocorreu um erro ao recuperar produtos. Status code = " +
+          response.status
+      );
+    }
+    return await response.json();
+  };
+
+  return useQuery({
+    queryKey: ["produtos"],
+    queryFn: () => recuperarProdutos(),
+    staleTime: 10_000,
+  });
+};
+export default useRecuperarProdutos;
